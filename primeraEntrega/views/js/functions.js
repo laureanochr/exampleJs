fde8fe78b3c580faf28df35119f41e9d32250fe6
Laureano Chr <laureanogrillo@gmail.com>
2022-07-21 12:25:45 -0300

    primera entrega

---
+++ primeraEntrega/views/js/functions.js
@@ -0,0 +1,20 @@
+function calculandoTurno(){
+    let nombreUsuario = prompt("Ingrese se nombre");
+    let numeroTurno = parseInt(prompt("Ingrese su numero de turno"));
+    let maxTurno = 10;
+
+    if(nombreUsuario && numeroTurno){
+    	if(numeroTurno < maxTurno){
+			for (numeroTurno; numeroTurno <= maxTurno; numeroTurno++) {
+				let resulTurno = maxTurno - numeroTurno;
+				alert ('Estimado '+nombreUsuario+', su numero de turno es '+numeroTurno+', aún le quedan '+resulTurno+' turnos disponibles ');
+			}
+	    	alert ('Estimado '+nombreUsuario+', usted ha alcanzado el numero máximo de turnos ('+maxTurno+'), Muchas gracias.');
+   	 	}else{
+   	 		alert ('Estimado '+nombreUsuario+', su numero máximo de turnos es de ('+maxTurno+').');
+   	 	}
+    }else{
+    	alert ('Debe completar todos los datos, recuerde que el turno tiene que ser un numero entero');
+    }
+ }
+
